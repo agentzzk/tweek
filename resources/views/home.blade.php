9f1d14dde331d79750bf4941a13ca11a9db4be17
Zain Zulfiqar <contact@zainzulfiqar.com>
2016-07-21 22:13:25 -0400

    Clean up project files; Restructure where needed

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -16,15 +16,10 @@
     <script src="//twemoji.maxcdn.com/twemoji.min.js"></script>
     <script>
         window.onload = function() {
-
-            // Set the size of the rendered Emojis
-            // This can be set to 16x16, 36x36, or 72x72
+            // Set the size of the rendered Emojis: 16x16, 36x36, or 72x72
             twemoji.size = '72x72';
-
-            // Parse the document body and
-            // insert <img> tags in place of Unicode Emojis
+            // Parse the document body and insert <img> tags in place of Unicode Emojis
             twemoji.parse(document.body);
-
         }
     </script>
 </head>
